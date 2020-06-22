ac5e876957fbbea413896548e6884ff97063a673
rockinrobin714 <robindykema@gmail.com>
2020-06-21 23:23:35 -0500

    form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import Img from "gatsby-image";
 import { useStaticQuery, graphql } from "gatsby";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  subject: "",
+  body: "",
+  status: "IDLE",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateFieldValue":
+      return { ...state, [action.field]: action.value };
+    case "updateStatus":
+      return { ...state, status: action.status };
+    case "reset":
+    default:
+      return INITIAL_STATE;
+  }
+};
 
 const AboutMe = () => {
   const data = useStaticQuery(graphql`
@@ -14,30 +34,77 @@ const AboutMe = () => {
       }
     }
   `);
+
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const setStatus = status => dispatch({ type: "updateStatus", status });
+  const updateFieldValue = field => event => {
+    dispatch({
+      type: "updateFieldValue",
+      field,
+      value: event.target.value,
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    setStatus("PENDING");
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(state),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        setStatus("SUCCESS");
+      })
+      .catch(error => {
+        console.error(error);
+        setStatus("ERROR");
+      });
+  };
+
   return (
     <section id="contact" className="half-row">
       <div className="text-box">
-        <form>
+        {state.status === "ERROR" && (
+          <p>Something went wrong. Please try again</p>
+        )}
+        <form onSubmit={handleSubmit}>
           <label>
             Name
-            <input />
-          </label>
-          <label>
-            Company
-            <input />
-          </label>
-          <label>
-            Number
-            <input />
+            <input
+              name="name"
+              onChange={updateFieldValue("name")}
+              type="text"
+              value={state.name}
+            />
           </label>
           <label>
             Email
-            <input />
+            <input
+              name="email"
+              onChange={updateFieldValue("email")}
+              type="email"
+              value={state.email}
+            />
           </label>
           <label>
-            Message
-            <textarea />
+            Subject
+            <input
+              name="subject"
+              onChange={updateFieldValue("subject")}
+              type="text"
+              value={state.subject}
+            />
           </label>
+          <label>
+            Body
+            <textarea
+              name="body"
+              onChange={updateFieldValue("body")}
+              value={state.body}
+            />
+          </label>
+          <button>Send</button>
         </form>
       </div>
       <div>
